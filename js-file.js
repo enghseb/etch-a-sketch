6a833c3ae1e99c370188340dc7c6441aed01d65b
Sebastian <enghseb@gmail.com>
2022-07-04 11:27:31 +0000

    Added ability to change number of squares per side, but  keeping width and height of grid

--- js-file.js
+++ js-file.js
@@ -1,21 +1,35 @@
-squarenum = 0
+function setCssProperty(variable, property) {
+    /*Changes a .css variable */
+    document.documentElement.style
+    .setProperty(variable, property)
+}
 
-function createSquares() {
+function createSquares(squaresPerSide) {
+    /*Makes sure width and height is correct
+        and creates one square */
     const square = document.createElement("div");
-    square.id = squarenum;
-    squarenum++
+
+    squareWidth = 400/squaresPerSide
+    squareHeight = 400/squaresPerSide
+    console.log(`${squareWidth}px`)
+    console.log(`${squareHeight}px`)
+    setCssProperty('--squareWidth-width', `${squareWidth}px`)
+    setCssProperty('--squareHeight-height',`${squareHeight}px`)
+
     const insertLocation = document.getElementById("grid");
     insertLocation.appendChild(square);
 }
 
-for (let squares = 0; squares < 256; squares++) {
-    createSquares()
+squaresPerSide = 35
+
+/* runs createSquare through a loop the same amount of times
+    as squares that should be created  */
+for (let createdSquares = 0; createdSquares < squaresPerSide*squaresPerSide; createdSquares++) {
+    createSquares(squaresPerSide)
 } 
 
-  let test = document.getElementById("grid");
-
+    //highlights the mouseenter target with color
   grid.addEventListener("mouseover", function( event ) {
-    // highlight the mouseenter target
     event.target.style.backgroundColor = 'black';;
-
   })
+
